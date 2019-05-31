e7c5486f199747de877f5b4108221e473f636f94
Jimmy Moon <ragingwind@gmail.com>
2019-05-31 22:12:48 +0900

    Add node lambda

---
+++ test/server.js
@@ -0,0 +1,48 @@
+const got = require('got');
+
+const ep = (pathname, query) => `http://localhost:3000/${pathname}${query ? query : ''}`
+const qs = params => Object.keys(params).map(k => `${k}=${params[k]}`).join('&')
+
+const opts = {
+  headers: {
+    Authorization: `Bearer ${process.env.KEY}`
+  },
+  timeout: false
+};
+
+const get = async (pathname, query) => {
+  const res = await got.get(ep(pathname, query), {
+    ...opts,
+    responseType: 'json'
+  })
+
+  return JSON.parse(res.body);
+}
+
+const post = async (subpath, body) => {
+  const res = await got.post(ep(subpath), {
+    ...opts,
+    body: body,
+  });
+
+  return JSON.parse(res.body);
+}
+
+(async () => {
+  const deployments = await get('deployments')
+  console.log('deployments', deployments);
+
+  const project = Object.values(deployments)[0];
+  
+  const config = await post('download', JSON.stringify({
+    uid: project.uid
+  }));
+
+  console.log('download', config);
+
+  const res = await post('deploy', JSON.stringify({
+    config
+  }));
+
+  console.log('deploy', res);
+})()
